bd944e664819a31e297552ac6cb9bf5573982a54
Joshua Smith <joshuasmith7278@gmail.com>
2024-04-20 21:28:32 -0400

    Created CSS file for Movie Page styling. Cleaner Movie Page file.

--- src/pages/movie.js
+++ src/pages/movie.js
@@ -1,19 +1,16 @@
 import React, {useState, useEffect} from 'react';
-import ReactDOM from 'react';
-import StarIcon from '@mui/icons-material/Star';
 import StarBorderIcon from '@mui/icons-material/StarBorder';
-import StarHalfIcon from '@mui/icons-material/StarHalf';
 import { Rating } from '@mui/material';
-import { getMovies } from '../components/axios';
-import DisplaySearchBar from '../components/header';
-import ReviewList from '../components/HomeRevDisp';
 import { getRevFromMID } from '../components/axios';
 import { useLocation } from 'react-router-dom';
 import PrevReviews from '../components/PrevReviews';
+import '../css/MoviePage.css';
 
 
 const Movie = () => {
     console.log("Movie Page renders")
+    
+    //Create state variables for the movie information
     const [movie, setMovie] = useState(
         {
             "id":null,
@@ -26,7 +23,8 @@ const Movie = () => {
     const location = useLocation();
 
 
-
+    //Set the movie state information once the location of the page renders
+    //LOOK INTO THIS!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     useEffect(()=>{
         if(location.state != null){
             setMovie({id:location.state.movieID, title:location.state.title, poster:location.state.poster})
@@ -35,8 +33,9 @@ const Movie = () => {
 
     }, [location.state])
 
+    
+    //Get reviews for the movie on the page once the movie info renders
     useEffect(()=>{
-        console.log("Trying to get reviews for " + movie.id)
         if(movie.id != null){
             getRevFromMID(movie.id).then(json=>{
                 setRevs(json)
@@ -48,92 +47,34 @@ const Movie = () => {
 
     }, [movie.id])
 
-    console.log(movie.poster)
-
-
-
-    const reviewPage = {
-        margin:"65px"
-    }
-
-    const reviewTitle = {
-        color:"white"
-    }
-
-    const starStyle = {
-        color:"#DAA520"
-    }
-
-    const reviewBox = {
-        display:'flex',
-        flexDirection:'column',
-        width:"550px",
-        marginTop:"10px"
-    
-    }
-
-    const textBox = {
-        borderRadius:"15px" ,
-        backgroundColor:'#202020',
-        border:"black",
-        color:"white",
-        fontSize:"18px",
-        padding:"10px"
-      
-    }
-
-
-    const reviewPostButton = {
-        marginTop:"15px",
-        width:"100px",
-        height:"30px",
-        backgroundColor:"#121212",
-        color:"white",
-        borderRadius:"10px",
-        border:"3px solid white"
-    }
-
-    const revPage = {
-        display:"flex",
-        marginTop:"30px",
-        marginLeft:"65px"
-    }
-
-    const mvImg = {
-        width:"500px",
-        maxHeight:"700px"
-    }
-
-    const movieDiv = {
-        marginRight:"80px"
-    }
-
-
+   
     const validateForm = (event) =>{
         event.preventDefault();
         let reviewRating = document.forms["reviewForm"]["half-rating"].value;
         console.log("SUBMITTING REVIEW")
     }
     
-    return(
-        <div style={revPage}>
+    console.log(movie.id)
 
-                <div style={movieDiv}>
-                    <h1 style={reviewTitle}>{movie.title}</h1>
-                    <img style={mvImg} src={movie.poster}/>
+    return(
+        <div className='revPage'>
+
+                <div className='movieDiv'>
+                    <h1 className='reviewTitle'>{movie.title}</h1>
+                    <img className='mvImg' src={movie.poster}/>
                 </div>
                 
                 <div id='reviewForm'>
-                    <h1 style={reviewTitle}>Post Review</h1>
+                    <h1 className='reviewTitle'>Post Review</h1>
                     <form name="reviewForm" onSubmit={validateForm}>
-                        <Rating name='half-rating' defaultValue={0} precision={0.5} emptyIcon={<StarBorderIcon style={starStyle}/>}/>
-                        <div style={reviewBox}>
-                            <textarea name="review" style={textBox} rows="10" cols={"20"}/>
-                            <button type="submit" style={reviewPostButton}>Post Review</button>
+                        <Rating name='half-rating' defaultValue={0} precision={0.5} emptyIcon={<StarBorderIcon className='starStyle'/>}/>
+                        <div className='reviewBox'>
+                            <textarea name="review" className='textBox' rows="10" cols={"20"}/>
+                            <button type="submit" className='reviewPostButton'>Post Review</button>
                         </div>
                     </form>
 
-                    <h1 style={reviewTitle}>Previous Reviews</h1>
+                    <h1 className='reviewTitle'>Previous Reviews</h1>
                     <PrevReviews reviews={revs}/>
 
                     
